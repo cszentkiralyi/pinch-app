6f672d68d00606c2cf067db996c151173046eab8
Chris Szentkiralyi <chris.szentkiralyi@gmail.com>
2019-11-06 07:02:41 -0700

    start of routing, extracted btn styles

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ const STYLES = {
   container: "p-4 my-8 bg-white text-black shadow-md ",
 
   input: "my-2 p-2 border-gray-400 focus:border-purple-400 text-lg ",
+  button_primary: "p-4 bg-purple-400 text-gray-100 font-weight-700 shadow-md ",
+  button_alt: "p-4 text-purple-400 font-weight-700 ",
   corner_button: "absolute inset-b-0 inset-r-0 m-8 rounded-full bg-purple-400 text-black text-3x font-weight-300 flex size-xl justify-center items-center "
 };
 
@@ -31,10 +33,11 @@ const NewEntryCard = {
         </div>
         <div className="flex relative justify-center items-center p-4 mt-8">
           {NBSP}
-          <div className="absolute inset-l-0 ml-4 p-4 text-purple-400 font-weight-700">
+          <a className={STYLES.button_alt + "block absolute inset-l-0 ml-4"}
+            href="#!/entry">
             more
-          </div>
-          <div className="absolute inset-r-0 ml-4 p-4 bg-purple-400 text-gray-100 font-weight-700 shadow-md">
+          </a>
+          <div className={STYLES.button_primary + "absolute inset-r-0 ml-4"}>
             enter
           </div>
         </div>
@@ -65,25 +68,62 @@ const RecentEntriesCard = {
           <div className="border-none border-b border-gray-300 p-2 mr-2">$12.29</div>
         </div>
         <div className="flex mt-2 justify-center items-center">
-          <div className="p-4 text-purple-400 font-weight-700">more</div>
+          <div className={STYLES.button_alt}>more</div>
         </div>
       </div>
     );
   }
-}
-;
+};
 
-const App = {
-  view: function App_view() {
+const HomeScreen = {
+  view: function HomeScreen_view() {
     return (
       <div className="h-100 bg-gray-100 text-rg font-weight-400">
         <div className={STYLES.header}>pinch</div>
         <NewEntryCard />
         <RecentEntriesCard />
-      </div>);
+      </div>
+    );
   }
 }
 
-//m.render($("#app-container"), "Hello world!");
-m.mount($("#app-container"), App);
+const EditScreen = {
+  view: function EditScreen_view() {
+    return (
+      <div className="h-100 bg-gray-100 text-rg font-weight-400">
+        <div className={STYLES.header}>
+          <a className="flex absolute inset-y-0 inset-l-0 justify-center items-center ml-2 p-4 text-gray-100 text-rg font-weight-700"
+             href="#!/">
+            back
+          </a>
+          pinch
+        </div>
+
+        <div className={STYLES.container}>
+          <div className="relative mb-8">
+            <input className={STYLES.input + "pl-8"} type="number" />
+            <div className="absolute inset-y-0 inset-l-0 ml-2 opacity-20 text-lg flex justify-center items-center">$</div>
+          </div>
+          <div className="mb-8">
+            <input className={STYLES.input} type="text" placeholder="description" />
+          </div>
+          <div className="mb-8">
+            <input className={STYLES.input + "w-100 bg-white"} type="date" />
+          </div>
+          <div className="flex relative justify-center items-center mt-8">
+            <div className={STYLES.button_primary + "mx-4 flex-grow text-center"}>
+              enter
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+m.route($("#app-container"), "/", {
+  "/": HomeScreen,
+  "/entry": EditScreen,
+  "/entry/:id": EditScreen
+});
 
